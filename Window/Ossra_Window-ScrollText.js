9e2ecb5f187057b56be66a11e2d3bebcf177ca52
ossra <54658971+ossra@users.noreply.github.com>
2019-10-11 01:22:09 -0700

    Add files via upload

--- Window/Ossra_Window-ScrollText.js
+++ Window/Ossra_Window-ScrollText.js
@@ -13,6 +13,10 @@
     tempAlias_WindowBase_initialize.call(this, x, y, width, height);
 
     this.__scrollSpeed = 0;
+    this.__scrollWait  = 120;
+    this.__scrollCount = 0;
+    this.__scrollMode  = 0;
+    this.__maxScroll   = 0;
 
   };
 
@@ -26,8 +30,20 @@
 
     tempAlias_WindowBase_update.call(this);
 
-    if (this.__scrollSpeed !== 0) {
-      this._windowContentsSprite.origin.x += -this.__scrollSpeed;
+    if (this.__scrollMode === 1) {
+      if (Math.abs(this._windowContentsSprite.origin.x) < this.__maxScroll) {
+        this._windowContentsSprite.origin.x += -this.__scrollSpeed;
+      } else {
+        this._windowContentsSprite.origin.x = 0;
+        this.__scrollMode = 2;
+      }
+    } else if (this.__scrollMode === 2) {
+      if (this.__scrollCount < this.__scrollWait) {
+        this.__scrollCount++;
+      } else {
+        this.__scrollMode = 1;
+        this.__scrollCount = 0;
+      }
     }
 
   };
@@ -66,7 +82,7 @@
 
     this._windowContentsSprite = new TilingSprite();
     this._windowContentsSprite.move(cPadding, cPadding, cWidth, cHeight);
-    this._windowContentsSprite.bitmap = new Bitmap(tWidth, cHeight);
+    this._windowContentsSprite.bitmap = new Bitmap(Math.max(tWidth * 2, cWidth), cHeight);
 
     this.addChildAt(this._windowContentsSprite, sIndex);
 
@@ -123,20 +139,25 @@
 
   Window_Base.prototype.initScroll = function(textState) {
 
-    var regexp = /^\[(-?\d+\.?\d?),\s+(.+)\]/;
+    var regexp = /^\[(-?\d+\.?\d?),\s*(\d+),\s*(.+)\]$/;
     var arr = regexp.exec(textState.text.slice(textState.index));
 
     if (arr) {
       textState.index += arr[0].length;
 
       var speed = parseFloat(arr[1]);
-      var width = this.contents.measureTextWidth(arr[2]);
+      var wait  = parseInt(arr[2]);
+      var width = this.contents.measureTextWidth(arr[3]);
       var index = textState.index + 1;
       var text  = textState.text;
 
+      this.__scrollWait = wait;
+      this.__maxScroll  = width + this.contents.fontSize;
+      this.__scrollMode = 2;
+
       this.createScrollContents(width, speed);
 
-      textState.text = text.slice(0, index) + arr[2] + text.slice(index);
+      textState.text = text.slice(0, index) + arr[3] + text.slice(index);
     } else {
       return '';
     }
